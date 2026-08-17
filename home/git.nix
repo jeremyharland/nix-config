9606cd604242b9606ab1a5972300a3f894c69692
Jeremy Harland <jeremy@getmosh.com>
2026-08-17 12:24:50 +1000

    update

--- home/git.nix
+++ home/git.nix
@@ -4,12 +4,10 @@
     enable = true;
 
     settings = {
-      user = gitUser;
+      user = gitUser // { signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfguAGDkLgRwurmbo/fS5+A7rXVCK5mkuJX6tfiH3PD"; };
 
       alias.lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       init.defaultBranch = "main";
-
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfguAGDkLgRwurmbo/fS5+A7rXVCK5mkuJX6tfiH3PD";
       gpg.format = "ssh";
       "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
       commit.gpgsign = true;
