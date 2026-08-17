5dd86961c1f34e032453962dd9b64b03b95dcc61
Jeremy Harland <jeremy@getmosh.com>
2026-08-17 12:23:25 +1000

    add git signing

--- home/git.nix
+++ home/git.nix
@@ -8,6 +8,11 @@
 
       alias.lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       init.defaultBranch = "main";
+
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfguAGDkLgRwurmbo/fS5+A7rXVCK5mkuJX6tfiH3PD";
+      gpg.format = "ssh";
+      "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      commit.gpgsign = true;
     };
   };
 }
